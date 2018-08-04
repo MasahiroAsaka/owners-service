24f0d01626720eef0c51af4403d8ed76160c369e
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-08-04 14:37:52 +0900

    Add Plan Show view
    Planの詳細画面の表示

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,4 +3,7 @@ class PlansController < ApplicationController
   def index
   end
 
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "plans#index"
   devise_for :owners
+  resources :plans, only:[:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
