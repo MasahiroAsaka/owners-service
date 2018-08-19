2250b483a099eaa36238c82df3b84157dac65617
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-08-19 15:12:03 +0900

    Change migration files

--- db/migrate/20180801082230_create_plans.rb
+++ db/migrate/20180801082230_create_plans.rb
@@ -1,16 +1,14 @@
 class CreatePlans < ActiveRecord::Migration[5.0]
   def change
     create_table :plans do |t|
-      t.text      :title,                  null: false
-      t.datetime  :closing_date,           null: false
-      t.integer   :registration_fee,       null: false
-      t.text      :text
-      t.text      :content_of_service
-      t.text      :benefits
-      t.integer   :capacity,               null: false
-      t.datetime  :wanted_period,          null: false
-      t.text      :contract_period
-      t.text      :plan_schedule
+      t.text      :title,               null: false
+      t.integer   :registration_fee,    null: false
+      t.string    :shop_name,           null: false
+      t.string    :place,               null: false
+      t.string    :closing_date_month,  null: false
+      t.string    :closing_date_day,    null: false
+      t.text      :contents,            null: false
+      t.text      :plan_detail,         null: false
       t.timestamps
     end
   end
