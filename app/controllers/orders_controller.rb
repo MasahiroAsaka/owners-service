85133bed0b017c9366c6051a9a1b7f33920902b0
MasahiroAsaka <pedestrem.paradiso@gmail.com>
2018-09-01 12:51:53 +0900

    select_course
    order処理のselect_course

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
+before_action  :set_plan, only: [:select_cources]
+before_action  :login_check, only: [:select_cources_2]
 
   def select_cources
+  end
+
+  def select_cources_2
   end
 
   def purchase_cvs
@@ -10,6 +15,17 @@ class OrdersController < ApplicationController
   end
 
   def confirming
+  end
+
+  def login_check
+    unless owner_signed_in?
+      redirect_to new_owner_session_path
+    end
+  end
+
+  private
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   resources :plans, only:[:index, :show, :new, :create] do
     get 'pay', on: :collection
   end
-  get '/orders/select_cources/:id', to: 'orders#select_cources'
-  get '/orders/purchase/cvs/:id', to: 'orders#purchase_cvs'
-  get '/orders/purchase/credit_card/:id', to: 'orders#purchase_credit_card'
-  get '/orders/purchase/confirming/:id', to: 'orders#confirming'
+  get   '/orders/select_course/:id',        to: 'orders#select_cources'
+  post  '/orders/select_course/:id',        to: 'orders#select_cources_2'
+  get   '/orders/purchase/cvs/:id',         to: 'orders#purchase_cvs'
+  get   '/orders/purchase/credit_card/:id', to: 'orders#purchase_credit_card'
+  get   '/orders/purchase/confirming/:id',  to: 'orders#confirming'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
